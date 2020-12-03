2345b2cd9b53b1ac0e303cee7845dc5fae39d6fc
Melissa Donegan <melissadonegan25@gmail.com>
2020-12-02 22:55:44 -0500

    clean up

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // API
-
 fetch("https://5dced59675f9360014c2642c.mockapi.io/EarlyYears")
 .then(function(response) {
 	return response.json();
@@ -17,8 +16,7 @@ function appendData(data) {
 		const img = document.createElement('img');
 		img.src = `${data[i].image}`;
 		const div = document.createElement('div');
-		div.innerHTML = 
-		// 'Image: ' + data[i].image + 
+		div.innerHTML =  
 		'Title: ' + data[i].title + 
 		'Name: ' + data[i].name + 
 		'Date: ' + data[i].date + 
@@ -27,41 +25,9 @@ function appendData(data) {
 		mainContainer.appendChild(div);
 	}
 }
-// 	const api_url =
-// 	    "https://5dced59675f9360014c2642c.mockapi.io/EarlyYears"
-// 			async function getapi(url) {
-// 	    const response = await fetch(url);
-// 	    var data = await response.json();
-// 	    console.log(data);
-// 	    if (response) {
-// 	      hideloader();
-// 	    }
-// 			show(data);
-// 	}
 
-// 	function hideloader() {
-// 		document.getElementById('loading').style.display = 'none'; 
-// 	}
 
-// 	getapi(api_url);
 
-// 	function show(data) {
-// 	    let tab =
-// 	        `<tr>
-// 	    <th>Title: </th>
-// 	    <th>Name: </th>
-// 	    <th>Date: </th>
-// 	    <th>Description: </th>
-// 			</tr>`;
 
-// 	for (let r of data.list) {
-// 	    tab += `<tr>
-// 	    <td>${r.image} </td>
-// 	    <td>${r.title} </td>
-// 	    <td>${r.name} </td>
-// 	    <td>${r.date} </td>
-// 	    <td>${r.description} </td>
-// 			</tr>`
-// 	}
-// 	document.getElementById("pictures").innerHTML = tab;
-// }
+
+
